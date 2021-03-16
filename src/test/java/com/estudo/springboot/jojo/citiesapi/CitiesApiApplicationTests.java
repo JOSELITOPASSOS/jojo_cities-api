600d0f819ad9994df65d48625380ba2b17564050
unknown <joselitomartinho@gmail.com>
2021-03-15 22:16:51 -0300

    Projeto inicial.

---
+++ src/test/java/com/estudo/springboot/jojo/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.springboot.jojo.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
