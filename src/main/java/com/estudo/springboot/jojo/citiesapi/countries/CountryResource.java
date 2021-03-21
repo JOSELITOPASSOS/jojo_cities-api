1b7b035363da4f3c4a8574a39ab09543bc61e378
Joselito Passos <joselitomartinho@gmail.com>
2021-03-20 21:27:35 -0300

    Criando api estados.

--- src/main/java/com/estudo/springboot/jojo/citiesapi/countries/CountryResource.java
+++ src/main/java/com/estudo/springboot/jojo/citiesapi/countries/CountryResource.java
@@ -1,7 +1,7 @@
-package com.estudo.springboot.jojo.citiesapi;
+package com.estudo.springboot.jojo.citiesapi.countries;
 
 import com.estudo.springboot.jojo.citiesapi.countries.Country;
-import com.estudo.springboot.jojo.citiesapi.repository.CountryRepository;
+import com.estudo.springboot.jojo.citiesapi.countries.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
